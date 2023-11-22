8625b560d276b8ffd8be5f04c95c79a21085ddb7
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-11-22 11:32:59 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -2,7 +2,29 @@
  *
  * Created by: Ihor Chernyshev
  * Created on: Nov 2023
- * This program ...
+ * This program make a 28BYJ-48 DC 5V stepper motor move
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+basic.forever(function() {
+    
+})
+// loop forever
+basic.forever(function () {
+  if (input.buttonIsPressed(Button.A) == true) {
+    // turn the motor 180 degrees
+    basic.showIcon(IconNames.Yes)
+    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
+    basic.showIcon(IconNames.Happy)
+  }
+
+  if (input.buttonIsPressed(Button.B) == true) {
+    // move car forwards and backwards
+    basic.showIcon(IconNames.Yes)
+    robotbit.StpCarMove(10, 48)
+    basic.pause(500)
+    robotbit.StpCarMove(-10, 48)
+    basic.showIcon(IconNames.Happy)
+  }
+})
