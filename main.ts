a74f3fcdbce1b767d8c6d57bb92e7e6fc43c9842
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-11-22 12:26:58 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -1,30 +1,35 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Ihor Chernyshev
- * Created on: Nov 2023
+ * Created by: Mr. Coxall
+ * Created on: Sep 2020
  * This program make a 28BYJ-48 DC 5V stepper motor move
 */
 
-// setup
-basic.showIcon(IconNames.Happy)
-basic.forever(function() {
-    
-})
-// loop forever
-basic.forever(function () {
-  if (input.buttonIsPressed(Button.A) == true) {
-    // turn the motor 180 degrees
-    basic.showIcon(IconNames.Yes)
-    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
-    basic.showIcon(IconNames.Happy)
-  }
+// variables
+let distance = 0
 
-  if (input.buttonIsPressed(Button.B) == true) {
-    // move car forwards and backwards
-    basic.showIcon(IconNames.Yes)
-    robotbit.StpCarMove(10, 48)
-    basic.pause(500)
-    robotbit.StpCarMove(-10, 48)
-    basic.showIcon(IconNames.Happy)
-  }
-})
+// setup
+basic.showIcon(IconNames.Heart)
+
+// loop forever
+while (true) {
+    if (input.buttonIsPressed(Button.A) == true) {
+        distance = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+        )
+        while (distance <= 10) {
+            // move car forwards and backwards
+            basic.showNumber(distance)
+            robotbit.StpCarMove(1,1)
+            basic.pause(500)
+            basic.showIcon(IconNames.Yes)
+            distance = sonar.ping(
+                DigitalPin.P1,
+                DigitalPin.P2,
+                PingUnit.Centimeters
+            )
+        }
+    }
+}
